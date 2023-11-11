50d689448e64c5044a61cd368b48c659a4587ad5
Matheus <matheu.mpf18@gmail.com>
2023-11-11 15:02:31 -0300

    Commit 2.0 > Creating functions in javaScript

---
+++ Script/faleConosco.js
@@ -0,0 +1,11 @@
+let chamarForm = document.querySelector(".fale-conosco");
+let mascaraForm = document.querySelector(".mascara-form");
+
+function faleConosco(){
+    chamarForm.style.left = "700px";
+    mascaraForm.style.visibility = "visible";
+}
+
+function sumirForm(){
+    formulario.style.left = "-600px";
+}
